2ab117112d789dd41937ddb2eb457680f6d16e8e
aatje-sy <1206993@student.roc-nijmegen.nl>
2025-05-24 21:07:11 +0200

    Game interface

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Kalima</title>
+</head>
+<body>
+    <main>
+        <div class="input-container">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+        </div>
+        <div class="input-container">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+        </div>
+        <div class="input-container">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+        </div>
+        <div class="input-container">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+            <input maxlength="1" type="text" name="" id="">
+        </div>
+
+    </main>
+
+</body>
+</html>

1556047366b7b5a8dfe6f1ebfc491a0fd12215f7
Ryzex <Ryzexgamer1@gmail.com>
2025-05-26 20:07:49 +0200

    added a login function and also dark light mode with the controllers added and everything to make 2 players in one place some of the styling

---
+++ app/Http/Controllers/turn.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class turn extends Controller
+{
+    public function turns()
+    {
+        if (session('turn') === 'player1') {
+            session()->put('turn', 'player2');
+        } else {
+            session()->put('turn', 'player1');
+        }
+        $araay = [1,0,2,0,0];
+
+
+        return view('index', ['araay' => $araay]);
+
+    }
+
+    public function words()
+    {
+
+        $woord = 'horny';
+
+
+
+
+    }
+}
